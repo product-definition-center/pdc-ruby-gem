d4ba9e03e04598149dba0f4b2b13ac56643ac774
Chuang Zhang <chuzhang@redhat.com>
2018-03-07 14:47:00 +0800

    Fix some issues for multi destination
    Correct spec file name, fixtures uri and some typos.

--- lib/pdc/v1/multi_destination.rb
+++ lib/pdc/v1/multi_destination.rb
@@ -2,7 +2,7 @@ module PDC
   module V1
     class MultiDestination < PDC::Base
       attributes :id, :destination_repo, :global_component,
-                 :base_product, :active, :origin_repo, :subscribers
+                 :active, :origin_repo, :subscribers
     end
   end
 end
