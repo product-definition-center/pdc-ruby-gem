df099291389ad4dea1e26dc248a4db6df2285cdf
Chuang Cao <chcao@redhat.com>
2018-03-20 16:22:39 +0800

    Add the allowed_push_targets for Product

--- lib/pdc/v1/product.rb
+++ lib/pdc/v1/product.rb
@@ -1,5 +1,5 @@
 module PDC::V1
   class Product < PDC::Base
-    attributes :name, :short, :active, :product_versions, :internal
+    attributes :name, :short, :active, :product_versions, :internal, :allowed_push_targets
   end
 end
